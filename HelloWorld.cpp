ca467195e36d09c95399ae179bd2b550e5e4ca6b
ZRobert <robertpayne@retrocatgames.com>
2014-05-11 17:54:21 -0400

    assignment

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+//Robert Payne
+//L02
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello World";
+	return 0;
+	
+}
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//Robert Payne
+//L02
+
+public class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}//This prints out "Hello World!" to the console
+
+}
